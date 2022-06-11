61ee547f13adb17af28c755aa8e6f948e27431f6
tiagoferreira18 <tiagoferreira18@gmail.com>
2022-06-11 08:27:46 -0300

    desafios

---
+++ introducao-js/aula02-contador/assets/scripts.js
@@ -0,0 +1,66 @@
+const currentNumberWrapper = document.getElementById('currentNumber')
+const subtracao = document.querySelector('#increment')
+const soma = document.querySelector('#decrement')
+
+
+let currentNumber = 0;
+
+
+/* Funcoes */
+
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function incrementColor(){
+    if(currentNumberWrapper.innerHTML > 0){
+        currentNumberWrapper.classList.remove("vermelho")
+        currentNumberWrapper.classList.add("azul")
+   
+    }else if(currentNumberWrapper.innerHTML == 0){
+        currentNumberWrapper.classList.remove("vermelho")
+        currentNumberWrapper.classList.remove("azul")
+    }
+}
+
+function decrementColor(){
+    if(currentNumberWrapper.innerHTML < 0){
+        currentNumberWrapper.classList.remove("azul")
+        currentNumberWrapper.classList.add("vermelho")
+    }else if(currentNumberWrapper.innerHTML == 0){
+        currentNumberWrapper.classList.remove("vermelho")
+        currentNumberWrapper.classList.remove("azul")
+    }
+}
+
+
+function decrement(){
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+
+/* Eventos de click */
+subtracao.addEventListener('click', ()=>{
+    increment()
+    incrementColor()
+   
+})
+
+soma.addEventListener('click', ()=>{
+    decrement()
+    decrementColor()
+   
+         
+})
+
+
+
+/* Contador maior que 10 */
+
+for( let i = 1, i<10; i++  ){
+
+}
+
+
