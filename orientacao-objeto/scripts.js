63a672cc9f9ccbccb2b8375ee9046ebbe1f249fe
tiagoferreira18 <tiagoferreira18@gmail.com>
2022-07-05 21:55:45 -0300

    atividades

--- orientacao-objeto/scripts.js
+++ orientacao-objeto/scripts.js
@@ -9,3 +9,92 @@ Ainda em ContaCorrente, faça com que o tipo seja 'conta corrente' por padrão;
 Crie uma classe-filha chamada ContaPoupanca que herda todos os parâmetros de ContaBancaria;
 Crie uma classe-filha chamada ContaUniversitaria que herda todos os parâmetros de ContaBancaria;
 Faça com que o método saque de ContaUniversitaria apenas seja capaz de sacar valores menores que 500 reais. */
+
+class ContaBancaria {
+    constructor(agencia, numero, tipo){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = 0;
+    }
+    get saldo(){
+        return this._saldo;
+    }
+
+    set saldo(valor){
+        this._saldo = valor;
+    }
+
+    sacar(valor){
+        if(valor > this._saldo){
+            return "Operação negada"
+        }
+        this._saldo = this._saldo - valor;
+        return this._saldo;
+    }
+
+    depositar(valor){
+        this._saldo = this._saldo + valor;
+        return this._saldo;
+
+    }
+
+}
+
+class ContaCorrente extends ContaBancaria{
+    constructor(agencia, numero, cartaoCredito){
+        super(agencia, numero);
+        this.tipo = 'corrente';
+        this.cartaoCredito = cartaoCredito;
+    }
+    get cartaoCredito(){
+        return this._cartaoCredito;
+    }
+    set cartaoCredito(valor){
+        this._cartaoCredito = valor;
+    }
+}
+
+class ContaPoupanca extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero);
+        this.tipo = 'poupança';
+       
+    }
+  
+}
+
+class ContaUniversitaria extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero);
+        this.tipo = 'universitaria';
+       
+    }
+
+    sacar(valor){
+        if(valor > 500){
+            return "Operação negada!"
+        }
+        this._saldo = this._saldo - valor;
+    }
+  
+}
+
+
+/* Teste para realizar no Console
+
+const minhaConta = new ContaCorrente(1, 234 , true)
+minhaConta.saldo
+minhaConta.depositar(1000)
+minhaConta.sacar(500)
+minhaConta.saldo
+minhaConta.sacar(600)
+
+const contaPoup = new ContaPoupanca(1, 22)
+
+const contaUni = new ContaUniversitaria(1, 22)
+contaUni.depositar(1000)
+contaUni.sacar(550)
+
+
+*/
