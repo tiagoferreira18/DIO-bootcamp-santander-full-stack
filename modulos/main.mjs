fe1af04248d6624a9d6d30882674dc89fe8283fe
tiagoferreira18 <tiagoferreira18@gmail.com>
2022-07-08 16:42:45 -0300

    Modulos

---
+++ modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Tiago', 33));
+console.log(mostraCidade('Tiago', 'Guarulhos'));
+console.log(mostraHobby('Tiago', 'Programar'));
