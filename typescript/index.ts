75d5f523ba5680f100e88e4a3024ee36a72c6af4
tiagoferreira18 <tiagoferreira18@gmail.com>
2022-07-12 16:46:57 -0300

    types e interface

--- typescript/index.ts
+++ typescript/index.ts
@@ -4,3 +4,32 @@ function soma(a: number, b: number){
 }
 
 console.log(soma(10, 5));
+
+
+//types
+//interfaces
+
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquatico';
+    executarRugido(alturaEmDecibeis: number): void;
+}
+
+interface IFelinos extends IAnimal{
+    visaoNoturna: boolean;
+}
+
+const animal: IAnimal = {
+    nome: 'elefante',
+    tipo: 'terrestre',
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+}
+
+animal.executarRugido(3);
+
+const felino: IFelinos = {
+    nome: 'Leão',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+    
+}
