18229d58f9885b3930496c7bc88577a5169516dc
tiagoferreira18 <tiagoferreira18@gmail.com>
2022-07-20 06:17:04 -0300

    unknown

--- introducao-typescript/app.ts
+++ introducao-typescript/app.ts
@@ -1,32 +1,21 @@
-// é possivel tipar a função assim evita erros no retorno da função 
+let anyEstaDeVolta: any;
 
-/* function somarValoresNumericos(numero1: number, numero2: number): number{
-    return numero1 + numero2;
+anyEstaDeVolta = 3;
+anyEstaDeVolta = '3';
+anyEstaDeVolta = 5;
+
+let stringTest: string = 'verificar';
+stringTest = anyEstaDeVolta;
+
+let unknownValor: unknown;
+unknownValor = 3;
+unknownValor = 'opa';
+unknownValor = true;
+unknownValor = 'Vai sim';
+
+let stringTest2: string = 'agora vai';
+stringTest2 = unknownValor;
+
+if(typeof unknownValor === 'string'){
+    stringTest2 = unknownValor;
 }
-
-console.log(somarValoresNumericos(1,3));
- */
-
-//  void é para rodar a função mais não esperar que ela devolva nada 
-function printaValoresNumericos(numero1: number, numero2: number): void{
-    console.log(numero1 + numero2);
-    
-}
-
-
-/* Callback */
-function somarValoresNumericosETratar(numero1: number, numero2: number, callback: (numero: number)=> number): number{
-    let resultado =  numero1 + numero2;
-    return callback(resultado);
-}
-
-function aoQuadrado(numero: number): number{
-    return numero * numero;
-}
-
-function dividirPorEleMesmo(numero: number): number{
-    return numero / numero;
-}
-
-console.log(somarValoresNumericosETratar(1, 3 , aoQuadrado));
-console.log(somarValoresNumericosETratar(1, 3 , dividirPorEleMesmo));
