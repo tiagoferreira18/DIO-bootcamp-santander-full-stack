7383167c7f6b776db475d7138210d7a2efcef4b2
tiagoferreira18 <tiagoferreira18@gmail.com>
2022-07-23 08:23:53 -0300

    Aula final do modulo 1

--- angular/course-manager/src/app/courses/course-list.component.ts
+++ angular/course-manager/src/app/courses/course-list.component.ts
@@ -14,21 +14,21 @@ export class CourseListComponent implements OnInit {
             {
                                id: 1,
                 name: 'Angular: Forms',
-                imageUrl:'',
+                imageUrl:'/assets/images/forms.png',
                 price: 99.99,
                 code: 'XPS-8796',
                 duration: 120,
-                rating: 5.4,
+                rating: 4.5,
                 releaseDate: 'December , 2, 2019'
             },
             {
                 id: 2,
                 name: 'Angular: HTTP',
-                imageUrl:'',
+                imageUrl:'/assets/images/http.png',
                 price: 49.99,
                 code: 'LKL-8796',
                 duration: 80,
-                rating: 4.4,
+                rating: 5,
                 releaseDate: 'December , 4, 2019'
 
             }
